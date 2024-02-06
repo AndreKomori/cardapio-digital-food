2eb29ee4d7345f2ee8cc3f93abb05de61fbf8210
Andre Komori <andrekomori28@gmail.com>
2024-02-06 14:58:16 -0300

    Primeiro commit do projeto

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import bag from "../../assets/bag.png";
+import Cart from "../cart/cart.jsx";
+import { Link } from "react-router-dom";
+
+function Navbar(props) {
+
+    function openSidebar() {
+        const event = new CustomEvent('openSidebar');
+        window.dispatchEvent(event);
+    }
+
+    return <div className="navbar">
+        <Link to={"/"}>
+            <img src={logo} className="logotipo" alt="Logotipo"></img>
+        </Link>
+    
+        {
+            props.showMenu &&  // Se a propriedade showMenu for exibida então(&&)
+                <div className="menu">
+                    <Link to="/historico">Histórico</Link>
+                    <button onClick={openSidebar} className="btn btn-red">
+                        <img src={bag} className="icon"></img>
+                        Sacola
+                    </button>
+                </div>
+        }
+
+        <Cart />
+    </div>
+}
+
+export default Navbar
